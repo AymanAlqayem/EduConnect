45a1ad26532df9bbfababe9e6058de232eead271
Ayman Nabil <nabilayman021@gmail.com>
2025-04-30 21:28:09 +0300

    Version 1

---
+++ app/src/main/java/com/tarificompany/android_project/SplashActivity.java
@@ -0,0 +1,54 @@
+package com.tarificompany.android_project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private ImageView logoImageView;
+
+    private TextView txtSlogan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        setUpViews();
+    }
+
+    /**
+     * setUpViews method that will initialise the hooks, and create the animation.
+     */
+    public void setUpViews() {
+        logoImageView = findViewById(R.id.logoImageView);
+        txtSlogan = findViewById(R.id.txtSlogan);
+
+        Animation logoAnim = AnimationUtils.loadAnimation(this, R.anim.logo_anim);
+        Animation textAnim = AnimationUtils.loadAnimation(this, R.anim.text_fade);
+
+        logoImageView.startAnimation(logoAnim);
+        txtSlogan.startAnimation(textAnim);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
+    }
+}
